eea41365665cb5f9e2d00eac295292e81be1cc55
Chetan Patel <Chetan@Chetans-MacBook-Air.local>
2021-07-09 02:31:23 -0400

    added the questions/answers in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,41 @@
 
+var questionsObject = [
+  {
+    
+    objectType: "MetaData",
+    currentScore: 0,
+    numberOfQuestions: 5,
+    userInitials: "",
+  },  
+  {   
+      objectType: "Question",
+      title: "What is HTML?",
+      selection: ["a styling langauge", "object oriented programming langauge", "whats an html?", "Hyper Text Markup Language"],
+      answer: {"d":"Hyper Text Markup Language"}
+  },
+  {   objectType: "Question",
+      title: "What is css?",
+      selection: ["object", "add's interactivity", "a type of API", "styling for html"],
+      answer: {"d":"styling for html"}
+  },
+  {   
+    objectType: "Question",
+      title: "an object is ____.",
+      selection: ["basically a function", "a type of array", "data type with state and properties", "all of the above"],
+      answer: {"c":"data type with state and properties"}
+  },
+  {   objectType: "Question",
+      title: "True or False: is a String an object?",
+      selection: ["True; its a collection of characters", "False; its a primitive value"],
+      answer: {"a":"True; its a collection of characters"}
+  },
+  {   objectType: "Question",
+      title: "What is a package manager",
+      selection: ["a thing to install manage and delete programs and its dependencies", "a terminal", 
+      "the console log", "an MSI file"],
+      answer: {"a":"a thing to install manage and delete programs and its dependencies"}
+  },
+
+
+
+];
